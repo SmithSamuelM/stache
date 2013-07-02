3cd70470923c4c10c2c57b3a35339719bdb671ab
Samuel M Smith <smith.samuel.m@gmail.com>
2013-07-02 16:08:25 -0600

    change name to staching to avoid pip conflict with Stache

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python
+""" setup.py
+
+    Basic setup file to enable pip install
+
+    python setup.py register  sdist upload 
+
+    
+"""
 
 import sys
 import os
@@ -7,17 +14,17 @@ from distutils.core import setup
 if sys.version_info < (2,5):
     raise NotImplementedError("Sorry, you need at least Python 2.5 to use stace.")
 
-import stache
+import staching
 
-setup(name='stache',
-      version=stache.__version__,
+setup(name='staching',
+      version=staching.__version__,
       description='Compact implementation of Mustache logic-less templating. Fork of Stache.',
-      long_description=stache.__doc__,
-      author=stache.__author__,
+      long_description=staching.__doc__,
+      author=staching.__author__,
       author_email='smith.samuel.m@gmail.com',
-      url='https://github.com/SmithSamuelM/stache',
-      py_modules=['stache'],
-      scripts=['stache.py'],
-      license=stache.__license__,
+      url='https://github.com/SmithSamuelM/staching',
+      py_modules=['staching'],
+      scripts=['staching.py'],
+      license=staching.__license__,
       platforms = 'any',
      )
